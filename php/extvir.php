9a1aaa55254b08933de9fdfeca624b372f86ab2a
matz <matz.sv86@gmail.com>
2017-02-28 14:29:47 +0100

    full push

---
+++ php/extvir.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BankNow - Accueil</title>
+	<link rel="stylesheet" type="text/css" href="../css/stylesheet.css" media="screen" />
+	<link href="https://fonts.googleapis.com/css?family=Sansita" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<header>
+	<img src="../include/logo.png" class="toplogo">
+</header>
+<body>
+	<h1 class="MainTitle">BankNow</h1>
+	<div class="infoContainer">
+		<div class="CurrentSold">
+			<p>3210</p>
+			<i class="fa fa-eur"></i>
+		</div>
+	</div>
+</body>
+</html>
